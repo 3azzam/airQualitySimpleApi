a7b759f371aea74e1b35ebd12c7da1cc30d82fd7
3azzam <azzammohamed923@gmail.com>
2024-02-12 05:10:55 +0200

    fix exception

--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -11,16 +11,21 @@ export class TasksService {
   @Cron(CronExpression.EVERY_MINUTE)
   async handleParisAirQualityCron() {
     console.log('running cron job');
-
-    const data: INearestCityResponse | ErrorResponse = await this.airQualityService.findNearestCity({
-      latitude: PARIS_LATITUDE,
-      longitude: PARIS_LONGITUDE,
-    });
-
-    this.airQualityService.addAirQuality({
-      country: data.country,
-      city: data.city,
-      pollution: data.current.pollution
-    });
+    try {
+      const data: INearestCityResponse = await this.airQualityService.findNearestCity({
+        latitude: PARIS_LATITUDE,
+        longitude: PARIS_LONGITUDE,
+      });
+  
+      this.airQualityService.addAirQuality({
+        country: data.country,
+        city: data.city,
+        pollution: data.current.pollution
+      });
+    }
+    catch(e) {
+      console.log("Error in cron >>>>>>>");
+      console.log(e);
+    }
   }
 }
